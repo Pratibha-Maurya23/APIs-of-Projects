007901648d0045a91b35b6ef3ff74c5ca90b7c30
PratibhaB.tech23 <pratibhacse20@gmail.com>
2025-12-30 01:47:15 +0530

    Delete Admission-automation-api directory

--- Admission-automation-api/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-const requireAuth = (req, res, next) => {
-  if (!req.session.studentId) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-  next();
-};
-
-export default  requireAuth;
